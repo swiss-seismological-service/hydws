e4316ce4d37ffaff15b00a3c26873682372b3337
sarsonl <laura.sarson@sed.ethz.ch>
2019-10-16 13:11:58 +0200

    [BUG] Fix tests to cope with borehole merging and loading data

--- hydws/db/tests/test_load_data.py
+++ hydws/db/tests/test_load_data.py
@@ -3,16 +3,19 @@
 Well related test facilities.
 """
 import os
+import datetime
 import unittest
 import subprocess
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, contains_eager
 
 from hydws.db import orm
 
-current_dir = os.path.abspath(__file__)
+current_dir = os.path.dirname(os.path.abspath(__file__))
 db_filename = os.path.join(current_dir, "data/test_loading.db")
 db_sqlite_string = f"sqlite:///{db_filename}"
+json_file_path = os.path.join(current_dir, 'data/borehole_file.json')
+json2_file_path = os.path.join(current_dir, 'data/borehole_file_updated.json')
 
 class LoadBoreholeTestCase(unittest.TestCase):
     """
@@ -20,26 +23,84 @@ class LoadBoreholeTestCase(unittest.TestCase):
     """
     db = None
 
-
     def test_load_merge_identical_file(self):
 
-        subprocess.run(['hydws-db-init', '-o', db_sqlite_string, '| > log_file.txt'])
+        subprocess.run(
+            ['hydws-db-init', '-o', db_sqlite_string,
+             '--logging-conf',
+             '/home/sarsonl/repos/hydws/hydws/config/logging.conf'],
+            stdout=subprocess.PIPE)
         engine = create_engine(db_sqlite_string, echo="debug")
         Session = sessionmaker(bind=engine)
 
-        subprocess.run(['hydws-data-import', db_sqlite_string])
+        subprocess.run(
+            ['hydws-data-import',
+             '--logging-conf',
+             '/home/sarsonl/repos/hydws/hydws/config/logging.conf',
+             db_sqlite_string, json_file_path])
         session = Session()
-        bh0 = session.query(orm.Borehole).one_or_none()
+        bh0 = session.query(orm.Borehole).\
+            options(contains_eager("_sections").
+                    contains_eager("_hydraulics")).one_or_none()
         session.close()
-        subprocess.run(['hydws-data-import', '--merge_only', db_sqlite_string])
+        subprocess.run(
+            ['hydws-data-import', '--merge_only',
+             '--logging-conf',
+             '/home/sarsonl/repos/hydws/hydws/config/logging.conf',
+             db_sqlite_string, json_file_path])
         engine = create_engine(db_sqlite_string, echo="debug")
 
         session = Session()
-        bh1 = session.query(orm.Borehole).one_or_none()
+        bh1 = session.query(orm.Borehole).\
+            options(contains_eager("_sections").
+                    contains_eager("_hydraulics")).one_or_none()
         session.close()
 
-        self.assertEqual(bh0, bh1)
+        self.assertEqual(len(bh0._sections[0]._hydraulics),
+                         len(bh1._sections[0]._hydraulics))
+        self.assertEqual(bh0._sections[0]._hydraulics[0].datetime_value,
+                         bh1._sections[0]._hydraulics[0].datetime_value)
+        self.assertEqual(bh0._sections[0]._hydraulics[-1].datetime_value,
+                         bh1._sections[0]._hydraulics[-1].datetime_value)
 
+    def test_load_updated_file(self):
+
+        subprocess.run(
+            ['hydws-db-init', '-o', db_sqlite_string,
+             '--logging-conf',
+             '/home/sarsonl/repos/hydws/hydws/config/logging.conf'],
+            stdout=subprocess.PIPE)
+        engine = create_engine(db_sqlite_string, echo="debug")
+        Session = sessionmaker(bind=engine)
+
+        subprocess.run(
+            ['hydws-data-import',
+             '--logging-conf',
+             '/home/sarsonl/repos/hydws/hydws/config/logging.conf',
+             db_sqlite_string, json_file_path])
+        session = Session()
+        bh0 = session.query(orm.Borehole).\
+            options(contains_eager("_sections").
+                    contains_eager("_hydraulics")).one_or_none()
+        session.close()
+        subprocess.run(
+            ['hydws-data-import', '--merge_only',
+             '--logging-conf',
+             '/home/sarsonl/repos/hydws/hydws/config/logging.conf',
+             db_sqlite_string, json2_file_path])
+        engine = create_engine(db_sqlite_string, echo="debug")
+
+        session = Session()
+        bh1 = session.query(orm.Borehole).\
+            options(contains_eager("_sections").
+                    contains_eager("_hydraulics")).one_or_none()
+        session.close()
+
+        self.assertEqual(len(bh1._sections[0]._hydraulics), 4)
+        self.assertEqual(bh0._sections[0]._hydraulics[0].datetime_value,
+                         bh1._sections[0]._hydraulics[0].datetime_value)
+        self.assertEqual(bh1._sections[0]._hydraulics[-1].datetime_value,
+                         datetime.datetime(2006, 12, 10, 1, 0))
 def suite():
     suite = unittest.makeSuite(LoadBoreholeTestCase, 'test')
     return suite
--- hydws/utils/json_schema/tests/test_import_json.py
+++ hydws/utils/json_schema/tests/test_import_json.py
@@ -35,7 +35,7 @@ class DataImportTestCase(unittest.TestCase):
            return_value=argparse.Namespace(
                db_url="sqlite:///{}/initialized.db".format(dirpath),
                assignids=True, overwrite_publicids=False,
-               publicid_uri="eth_uri/",
+               publicid_uri="eth_uri/", merge_only=False,
                data_file=opened_data, path_logging_conf=None, version=None))
     @patch('hydws.db.load_data.sys.exit')
     def test_data_import(self, sys_exit, parse_args):
