a19aecbbea795c92e2e95fef2bd4cbeb7b1b4377
sarsonl <laura.sarson@sed.ethz.ch>
2020-09-11 12:47:45 +0200

    [BUG] trying to fix adding samples to existing borehole

--- hydws/db/load_data.py
+++ hydws/db/load_data.py
@@ -174,6 +174,7 @@ class HYDWSLoadDataApp(App):
                                     section._hydraulics.remove(sample)
                                     sample.boreholesection_oid = None
                                     sample._section = None
+                                    session.flush()
                                     print("section", sample.boreholesection_oid, sample._section)
                                     sample._section = section_existing
                                     section_existing._hydraulics.append(sample)
