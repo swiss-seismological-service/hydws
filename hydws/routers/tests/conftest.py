8e53042666e3c57f8838aa3d6bc7ed9be3c6f589
Nicolas Schmid <nicolas@breiten.ch>
2024-04-18 17:15:57 +0200

    tests: correctly initialize database schema before running tests

--- hydws/routers/tests/conftest.py
+++ hydws/routers/tests/conftest.py
@@ -1,7 +1,22 @@
 import pytest
 from httpx import ASGITransport, AsyncClient
 
+from hydws.database import sessionmanager
+from hydws.datamodel.base import ORMBase
 from hydws.main import app
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def init():
+    if sessionmanager._engine:
+        async with sessionmanager.connect() as con:
+            await con.run_sync(ORMBase.metadata.create_all)
+
+    yield
+
+    if sessionmanager._engine is not None:
+        # Close the DB connection
+        await sessionmanager.close()
 
 
 @pytest.fixture(scope="session")
