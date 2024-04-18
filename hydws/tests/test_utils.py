a677dfdfb4a83a0624ba3a9a957d4ba6d652f007
Nicolas Schmid <nicolas@breiten.ch>
2024-04-18 16:11:47 +0200

    tests: for the new util functions merging hydraulic samples

--- hydws/tests/test_utils.py
+++ hydws/tests/test_utils.py
@@ -9,6 +9,9 @@ from hydws.utils import hydraulics_to_json, merge_hydraulics
 def test_real_values_to_json():
     # Create a sample DataFrame with real value columns
     data = {
+        'datetime_value': ['2021-01-01T00:00:00',
+                           '2021-01-01T00:01:00',
+                           '2021-01-01T00:02:00'],
         'depth': [1, 2, 3],
         'toppressure_value': [10.1, 20.2, 30.3],
         'toppressure_uncertainty': [0.1, 0.2, 0.3],
@@ -22,6 +25,7 @@ def test_real_values_to_json():
     # Test with default arguments
     expected_result = [
         {
+            'datetime': {'value': '2021-01-01T00:00:00'},
             'depth': 1,
             'toppressure': {
                 'value': 10.1, 'uncertainty': 0.1},
@@ -31,6 +35,7 @@ def test_real_values_to_json():
                 'value': 100.01, 'uncertainty': 1.0},
         },
         {
+            'datetime': {'value': '2021-01-01T00:01:00'},
             'depth': 2,
             'toppressure': {
                 'value': 20.2, 'uncertainty': 0.2},
@@ -40,6 +45,7 @@ def test_real_values_to_json():
                 'value': 200.02, 'uncertainty': 2.0},
         },
         {
+            'datetime': {'value': '2021-01-01T00:02:00'},
             'depth': 3,
             'toppressure': {
                 'value': 30.3, 'uncertainty': 0.3},
@@ -55,16 +61,19 @@ def test_real_values_to_json():
     expected_result = [
         {
             'depth': 1,
+            'datetime': {'value': '2021-01-01T00:00:00'},
             'topflow': {
                 'value': 100.01, 'uncertainty': 1.0},
             'toppressure': {
                 'value': 10.1, 'uncertainty': 0.1}}, {
             'depth': 2,
+            'datetime': {'value': '2021-01-01T00:01:00'},
             'topflow': {
                 'value': 200.02, 'uncertainty': 2.0},
             'toppressure': {
                 'value': 20.2, 'uncertainty': 0.2}}, {
             'depth': 3,
+            'datetime': {'value': '2021-01-01T00:02:00'},
             'topflow': {
                 'value': 300.03, 'uncertainty': 3.0},
             'toppressure': {
@@ -79,6 +88,34 @@ def test_real_values_to_json():
     # Test with empty DataFrame
     empty_df = pd.DataFrame()
     assert hydraulics_to_json(empty_df) == []
+
+
+def test_merge_hydraulics_simple():
+    merged = merge_hydraulics(df1, df2)
+    pd.testing.assert_frame_equal(merged, result1)
+
+
+def test_merge_hydraulics_gap():
+    merged = merge_hydraulics(df1, df3)
+    pd.testing.assert_frame_equal(merged, result2)
+
+
+def test_merge_hydraulics_overlapping():
+    merged = merge_hydraulics(df1, df4)
+    pd.testing.assert_frame_equal(merged, result3)
+
+
+def test_merge_hydraulics_large():
+    merged_1 = merge_hydraulics(df1, df5, limit=300)
+    merged_2 = merge_hydraulics(df1, df5, limit=299)
+
+    pd.testing.assert_frame_equal(merged_1, result4)
+    pd.testing.assert_frame_equal(merged_2, result5)
+
+
+def test_merge_hydraulics_realvalues():
+    merged = merge_hydraulics(df6, df7)
+    pd.testing.assert_frame_equal(merged, result6)
 
 
 CSV_DATA_1 = """datetime_value,toppressure_value
@@ -140,6 +177,36 @@ CSV_DATA_5 = """datetime_value,topflow_value
 2021-01-01T00:20:00,15.0
 2021-01-01T00:25:00,16.0
 2021-01-01T00:30:00,17.0
+"""
+
+CSV_DATA_6 = """
+datetime_value,toppressure_value,toppressure_uncertainty,topflow_value
+2021-01-01T00:00:00,1.0,0.1,11.0
+2021-01-01T00:00:30,1.0,0.1,12.0
+2021-01-01T00:01:00,2.0,0.2,13.0
+2021-01-01T00:01:30,2.0,0.2,14.0
+2021-01-01T00:02:00,3.0,0.3,15.0
+2021-01-01T00:02:30,3.0,0.3,16.0
+2021-01-01T00:03:00,4.0,0.4,17.0
+2021-01-01T00:03:30,4.0,0.4,18.0
+2021-01-01T00:04:00,5.0,0.5,19.0
+2021-01-01T00:04:30,5.0,0.5,20.0
+2021-01-01T00:05:00,6.0,0.6,21.0
+"""
+
+CSV_DATA_7 = """
+datetime_value,toppressure_value,topflow_value,topflow_uncertainty
+2021-01-01T00:00:00,1.0,11.0,1.0
+2021-01-01T00:00:30,1.0,12.0,2.0
+2021-01-01T00:01:00,2.0,13.0,3.0
+2021-01-01T00:01:30,2.0,14.0,4.0
+2021-01-01T00:05:00,3.0,15.0,5.0
+2021-01-01T00:05:30,3.0,16.0,6.0
+2021-01-01T00:06:00,4.0,17.0,7.0
+2021-01-01T00:06:30,4.0,18.0,8.0
+2021-01-01T00:07:00,5.0,19.0,9.0
+2021-01-01T00:07:30,5.0,20.0,10.0
+2021-01-01T00:08:00,6.0,21.0,11.0
 """
 
 RESULT_1 = """datetime_value,toppressure_value,topflow_value
@@ -230,6 +297,21 @@ RESULT_5 = """datetime_value,toppressure_value,topflow_value
 2021-01-01T00:30:00,NaN,17.0
 """
 
+RESULT_6 = """
+datetime_value,toppressure_value,topflow_value,topflow_uncertainty
+2021-01-01T00:00:00,1.0,11.0,1.0
+2021-01-01T00:00:30,1.0,12.0,2.0
+2021-01-01T00:01:00,2.0,13.0,3.0
+2021-01-01T00:01:30,2.0,14.0,4.0
+2021-01-01T00:05:00,3.0,15.0,5.0
+2021-01-01T00:05:30,3.0,16.0,6.0
+2021-01-01T00:06:00,4.0,17.0,7.0
+2021-01-01T00:06:30,4.0,18.0,8.0
+2021-01-01T00:07:00,5.0,19.0,9.0
+2021-01-01T00:07:30,5.0,20.0,10.0
+2021-01-01T00:08:00,6.0,21.0,11.0
+"""
+
 df1 = pd.read_csv(io.StringIO(CSV_DATA_1),
                   index_col='datetime_value', parse_dates=True)
 df2 = pd.read_csv(io.StringIO(CSV_DATA_2),
@@ -240,6 +322,11 @@ df4 = pd.read_csv(io.StringIO(CSV_DATA_4),
                   index_col='datetime_value', parse_dates=True)
 df5 = pd.read_csv(io.StringIO(CSV_DATA_5),
                   index_col='datetime_value', parse_dates=True)
+df6 = pd.read_csv(io.StringIO(CSV_DATA_6),
+                  index_col='datetime_value', parse_dates=True)
+df7 = pd.read_csv(io.StringIO(CSV_DATA_7),
+                  index_col='datetime_value', parse_dates=True)
+
 result1 = pd.read_csv(io.StringIO(RESULT_1),
                       index_col='datetime_value', parse_dates=True)
 result2 = pd.read_csv(io.StringIO(RESULT_2),
@@ -250,26 +337,5 @@ result4 = pd.read_csv(io.StringIO(RESULT_4),
                       index_col='datetime_value', parse_dates=True)
 result5 = pd.read_csv(io.StringIO(RESULT_5),
                       index_col='datetime_value', parse_dates=True)
-
-
-def test_merge_hydraulics_simple():
-    merged = merge_hydraulics(df1, df2)
-    pd.testing.assert_frame_equal(merged, result1)
-
-
-def test_merge_hydraulics_gap():
-    merged = merge_hydraulics(df1, df3)
-    pd.testing.assert_frame_equal(merged, result2)
-
-
-def test_merge_hydraulics_overlapping():
-    merged = merge_hydraulics(df1, df4)
-    pd.testing.assert_frame_equal(merged, result3)
-
-
-def test_merge_hydraulics_large():
-    merged_1 = merge_hydraulics(df1, df5, limit=300)
-    merged_2 = merge_hydraulics(df1, df5, limit=299)
-
-    pd.testing.assert_frame_equal(merged_1, result4)
-    pd.testing.assert_frame_equal(merged_2, result5)
+result6 = pd.read_csv(io.StringIO(RESULT_6),
+                      index_col='datetime_value', parse_dates=True)
